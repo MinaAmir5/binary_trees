c6e073652b6994f152c732a2576db3315317a947
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-11-30 12:52:49 +0200

    update

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,7 +9,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left = NULL && node->right == NULL)
+	if (node->right == NULL && node->left == NULL)
 		return (1);
 	else
 		return (0);
