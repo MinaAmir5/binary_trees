2d1b0f02f560e76c9f68b54b6238759afd430a01
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-11-30 13:23:10 +0200

    update

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -26,7 +26,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	else
 		return (binary_tree_is_leaf(tree) +
-			binary_tree_leaves(tree->right) +
-			binary_tree_leaves(tree->left));
+			binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right));
 
 }
